a99208a980a0d37a945e50f646c2119976f9be9c
jessicaann <jessicaecottrell@gmail.com>
2017-05-10 20:41:11 -0700

    push before singapore

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
 
-/*function watchSubmit() {
-  $('.js-search-form').submit(function(e) {
-    e.preventDefault();
-    $(".search-results-container").removeClass('hidden');
-    var userSearchTerm = $(this).find('.js-query').val();
-    getGeniusSearchData (userSearchTerm, displayGeniusSearchData);
-  });
-}
-
-$(function(){watchSubmit();}); */
-
-
-
 /*Yandex Get List of Languages*/
-function getListLangs (callback) {
-var getLangSettings = {
-  url: "https://translate.yandex.net/api/v1.5/tr.json/getLangs?",
-  key: "trnsl.1.1.20170502T035123Z.42da7472a927a423.30098ed9aeffb741e96cc5613005d8db93c70188",
-  ui: "en",
-  callback: "jsonp",
-  method: "GET",
- }
-$.ajax(getLangSettings);
+function getListLangs (successCallback) {
+  console.log('getListLangs');
+  var getLangSettings = {
+    url: "https://translate.yandex.net/api/v1.5/tr.json/getLangs",
+    data: {
+      key: "trnsl.1.1.20170502T035123Z.42da7472a927a423.30098ed9aeffb741e96cc5613005d8db93c70188",
+      ui: "en"
+    },
+    jsonp: "callback",
+    dataType: "jsonp",
+    method: "GET",
+    success: successCallback
+  }
+  $.ajax(getLangSettings);
 }
 
 function displayListLangs (data) {
+  console.log(data.langs);
 	var resultElement = '';
 	if (data.langs) {
 		var keys = Object.keys(data.langs);
@@ -36,28 +29,37 @@ function displayListLangs (data) {
 	$('.js-choose-lang').html(resultElement);
 }
 
-$('body').onload = function watchPageLoad() {
-  getListLangs();
-  displayListLangs();
+/*Yandex Translate*/
+function getTranslation (successCallback) {
+  var translationURL ='https://translate.yandex.net/api/v1.5/tr.json/translate';
+  var getTranslationSettings = {
+    url: translationURL,
+    data: {
+      key: "trnsl.1.1.20170502T035123Z.42da7472a927a423.30098ed9aeffb741e96cc5613005d8db93c70188",
+    	format: "text",
+    	text: "Please don't touch my hair",
+    	lang: "es",
+      options: 1,
+    },
+    jsonp: "callback",
+    dataType: "jsonp",
+    method: "GET",
+    success: successCallback
+  } 
+
+  $.ajax(getTranslationSettings);
+};
+
+function displayTranslation(data){
+  console.log(data);
 }
 
-/*Yandex Translate*/
-function getTranslation (callback) {
-var translationURL ='https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20170502T035123Z.42da7472a927a423.30098ed9aeffb741e96cc5613005d8db93c70188';
-var getTranslationSettings = {
-  url: translationURL,
-  data: {
-  	format: text,
-  	text: "Please don't touch my hair",
-  	lang: en,
-    options: 1,
-  },
-  method: "GET",
-  callback: jsonp,
-} 
+function watchPageLoad() {
+  getListLangs(displayListLangs);
+  getTranslation(displayTranslation);
+}
 
-$.ajax(getTranslationSettings);
-};
+$(watchPageLoad);
 
 /* use the same language selection to feed into a second translate function
 to translate the song lyrics automatically*/
